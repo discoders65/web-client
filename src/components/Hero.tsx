f19e08d95b4b621903b6d14ff688b09d5de88ce7
HurasAdam <huradam@wp.pl>
2024-04-26 08:13:58 +0200

    Feat: Added mobile devices styling for Hero section component.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,33 +4,37 @@ import Icons from "../Constants/icons";
 
 const Hero: React.FC = () => {
   return (
-    <section className=" bg-[#0b4127] w-full flex pt-3 justify-center ">
-      <div className="flex  justify-between  w-[1380px] ">
-        <div className=" flex-1 flex flex-col   gap-4 py-40">
-          <span className="text-5xl font-semibold text-slate-100 ">
+    <section className=" bg-[#0b4127] w-full flex  pt-3 justify-center px-12  2xl:px-0 ">
+      <div className="flex justify-between w-[1380px] ">
+        <div className="flex-1 flex flex-col py-20  gap-4 lg:py-40  ">
+          <span className="text-center text-4xl xl:text-left lg:text-5xl font-semibold text-slate-100 font-heroSectionFont ">
             Experience top-tier grooming.
           </span>
-          <span className="text-5xl font-semibold text-slate-100 ">
-            Let us pamper your furry friend
+          <span className="text-center text-4xl xl:text-left lg:text-5xl font-semibold text-slate-100 font-heroSectionFont">
+            Let us pamper your furry
           </span>
-          <span className="text-5xl font-semibold text-slate-100 ">
-            to perfection.
+          <span className="text-center text-4xl xl:text-left lg:text-5xl font-semibold text-slate-100 font-heroSectionFont">
+            friend to perfection.
           </span>
 
-          <span className="text-2xl font-semibold text-gray-300 ">
+          <span className="text-center text-4xl lg:text-left lg:text-2xl font-semibold text-gray-300 ">
             Join our satisfied clients today!
           </span>
-          <div className="w-full ">
-            <form className="w-full flex p-0 m-0 ">
+          <div className="w-full mt-14 lg:mt-4 ">
+            <form className="w-full gap-y-4 flex  flex-col lg:gap-y-0 lg:flex-row p-0 m-0 ">
               <input
                 type="text"
-                className="w-full py-2.5 px-2.5  rounded-l-sm outline-none"
+                className="w-full py-2.5 px-2.5 rounded-sm lg:rounded-none lg:rounded-l-sm outline-none "
               />
-              <button className="bg-green-500 border-none px-6 rounded-r-sm hover:bg-green-600">
-                <img src={Icons.SearchIcon} className="w-7 h-auto" alt="" />
+              <button className="bg-green-500 border-none px-6 py-2 lg:py-0 lg:rounded-r-sm hover:bg-green-600">
+                <img
+                  src={Icons.SearchIcon}
+                  className="w-7 h-auto"
+                  alt="search"
+                />
               </button>
             </form>
-            <div className="flex py-4 gap-4">
+            <div className="hidden lg:flex py-4 gap-4 ">
               <span className="text-slate-200 font-bold"> Popular:</span>
               <ul className="flex gap-2.5">
                 <li className="border text-sm px-2 py-1 text-slate-200 font-semibold rounded-full cursor-pointer hover:bg-slate-100 hover:text-black transition-all ">
@@ -47,11 +51,11 @@ const Hero: React.FC = () => {
           </div>
         </div>
 
-        <div className="w-1/2 flex justify-end ">
+        <div className="hidden lg:flex w-1/2  justify-end  ">
           <img
             src={images.HeroImage}
             alt="dog-photo"
-            className="w-[400px] h-auto"
+            className="w-auto h-auto"
           />
         </div>
       </div>
